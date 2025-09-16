2ea12708a781eb2ca0e390b4bc4a7099a920bb68
DreggRaulCabansay-UCM <dreggcabansay@gmail.com>
2025-09-16 08:45:33 +0800

    Add files via upload

---
+++ Chapter_1/Comments.java
@@ -0,0 +1,16 @@
+/**
+ * This is a Javadoc comment.
+ * It explains the class below.
+ */
+public class Comments {
+    public static void main(String[] args) {
+        // This is a single-line comment
+        System.out.println("Hello from single-line comment example!");
+
+        /*
+         * This is a multi-line comment.
+         * Useful for longer explanations.
+         */
+        System.out.println("Hello from multi-line comment example!");
+    }
+}
---
+++ Chapter_1/FavoriteSong.java
@@ -0,0 +1,8 @@
+public class FavoriteSong {
+    public static void main(String[] args) {
+        System.out.println("Standin' tall over my grave");
+        System.out.println("Make sure it's filled with all the people we supposedly saved");
+        System.out.println("Make sure you throw the plaques in and all the money we made");
+        System.out.println("I can't put them on my wall, that shit just ain't my taste");
+    }
+}
